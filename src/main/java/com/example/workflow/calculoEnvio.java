930a7775e5b5967a22ecdd3c6833de07161481fb
Luis Perez <133932599+xLuisPc@users.noreply.github.com>
2025-05-28 15:26:53 -0500

    Add files via upload
    Comienzo del proyecto

---
+++ src/main/java/com/example/workflow/calculoEnvio.java
@@ -0,0 +1,7 @@
+package com.example.workflow;
+
+//se recibe los productos, nacional o internacional, informacion del comprador, y se calcula con la base a cual enviara
+
+public class calculoEnvio {
+    
+}
